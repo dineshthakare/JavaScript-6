c0ebe0bb9d6d13f29b0a0eda1c9fede022a20df6
Dinesh Thakare <dineshthakare318@gmail.com>
2022-10-07 16:35:37 +0530

    jsproj27

---
+++ file.js
@@ -0,0 +1,55 @@
+let prog={
+  val1:"JavaScript",
+  val2:"Python",
+  val3:"Ruby",
+  val4:"Go",
+  val9:"languages",
+  val5:"ischallenging",
+  val6:"isrewarding",
+  val7:"difficult",
+  val8:"isfun",
+
+  fun1:
+  {
+      arr:["JavaScript","Python","Ruby","Go"]
+  },
+  val:2,
+  val10:true,
+  val11:7,
+  val12:true,
+
+
+  getval()
+  {
+      let a=this.val1
+      let b=this.val2
+      let c=this.val3
+      let i=this.val4
+      let j=this.val9
+      let k=this.val5
+      let l=this.val6
+      let m=this.val7
+      let n=this.val8
+      let d=this.fun1.arr
+      let e=this.val
+      let f=this.val10
+      let g=this.val11
+      let h=this.val12
+      console.log(a)
+      console.log(b)
+      console.log(c)
+      console.log(i)
+      console.log(j)
+      console.log(k)
+      console.log(l)
+      console.log(m)
+      console.log(n)
+      console.log(d)
+      console.log(e,f)
+      console.log(g)
+      console.log(h)
+      
+
+      }
+}
+prog.getval()
